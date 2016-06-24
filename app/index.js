e82d873536f3bf22f4aefa3bb90cf5eb9fe2fe9f
boazblake <boazblake@gmail.com>
2016-06-24 06:04:46 -0500

    testing fir deploy to heroku

--- app/index.js
+++ app/index.js
@@ -6,7 +6,13 @@ if (module.hot) {
 }
 
 const HelloWorld = () => (
-  <div>App made with Feathers, React, and Webpack</div>
+  <div>
+  <h2>Another App made with Feathers, React, and Webpack </h2>
+  <img src="./favicon.ico"/>
+  <img width="100px"  src="https://facebook.github.io/react/img/logo.svg"/>
+  <img width="200px" src="https://cdn-images-1.medium.com/max/1422/1*A-_KrEvMuiH7dlwshFw5aw.png"/>
+  </div>
+
 );
 
-render(<HelloWorld />, document.getElementById('app'));
+render(<HelloWorld />, document.querySelector('#app'));
